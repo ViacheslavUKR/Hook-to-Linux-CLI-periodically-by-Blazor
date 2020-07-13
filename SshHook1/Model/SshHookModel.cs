e1a0c69879d1a2cd795030cdce0db2838d85da0a
burgas275 <burgas275@gmail.com>
2020-07-14 02:45:30 +0300

    Add project files.

---
+++ SshHook1/Model/SshHookModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SshHook1.Model
+{
+    public class SshHookModel
+    {
+        [System.ComponentModel.DataAnnotations.Required]
+        public string Ip { get; set; }
+        [System.ComponentModel.DataAnnotations.Required]
+        public string Login { get; set; }
+        [System.ComponentModel.DataAnnotations.Required]
+        public string Pass { get; set; }
+        [System.ComponentModel.DataAnnotations.Required]
+        public string Command { get; set; }
+        public string Seconds { get; set; }
+    }
+}
